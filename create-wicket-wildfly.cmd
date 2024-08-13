b328b47acef80ed6a07e830820ebc0269c03ada9
Ulrich Fechtelhoff <ulrich.fechtelhoff@iqtig.org>
2024-08-13 17:07:00 +0200

    Small fixes and improvements.

--- create-wicket-wildfly.cmd
+++ create-wicket-wildfly.cmd
@@ -6,7 +6,7 @@ echo # Start - %~n0%~x0 (%0)
 echo ###############################################################################
 echo.
 
-mvn archetype:generate --batch-mode -DarchetypeGroupId=de.fechtelhoff -DarchetypeArtifactId=maven-wicket-template -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=%1 -DartifactId=%2 -Dpackage=%3
+mvn archetype:generate --batch-mode -DarchetypeGroupId=de.fechtelhoff -DarchetypeArtifactId=maven-wicket-wildfly-template -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=%1 -DartifactId=%2 -Dpackage=%3
 
 echo.
 echo ###############################################################################
--- src/main/resources/archetype-resources/src/main/java/web/gui/panel/FooterPanel.java
+++ src/main/resources/archetype-resources/src/main/java/web/gui/panel/FooterPanel.java
@@ -1,5 +1,6 @@
 package ${package}.web.gui.panel;
 
+import java.time.LocalDate;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
@@ -14,7 +15,9 @@ public class FooterPanel extends Panel {
 	protected void onInitialize() {
 		super.onInitialize();
 
-		final String copyright = "Copyright &copy; 2023 by Ulrich Fechtelhoff";
+		final int year = LocalDate.now().getYear();
+		final String copyright = String.format("Copyright &copy; %d by Ulrich Fechtelhoff", year);
+
 		add(new Label("copyright", Model.of(copyright)).setEscapeModelStrings(false));
 	}
 }
